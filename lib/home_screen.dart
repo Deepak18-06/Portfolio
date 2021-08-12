d56fcab3f23c0cdd372066424f64cada82ac9606
Deepak kumar Prajapat <it1716.iiitbhopal@gmail.com>
2021-08-12 22:40:28 +0530

    top section of portfolio which include photo,name and logo

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'sections/topSection/top_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TopSection(),
+          ],
+        ),
+      ),
+    );
   }
 }
